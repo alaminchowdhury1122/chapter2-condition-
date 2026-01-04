107647b54ece313e666cc99de5edd3584f1f376b
alaminchowdhury1122 <alonealamin109@gmail.com>
2026-01-05 00:07:16 +0600

    compair2

---
+++ compair2.py
@@ -0,0 +1,10 @@
+x = int (input("what's X? "))
+y = int (input("what's y? "))
+
+
+if x < y: 
+    print("x is less then y")
+    
+else: 
+    print("y is bigger")
+    
--- compare.py
+++ compare.py
@@ -1,5 +1,5 @@
 x = int(input("what's x? "))
-y = int(input("what's y?"))
+y = int(input("what's y? "))
 
 
 if x < y:
